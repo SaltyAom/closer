aa06c2e6e0bbdd1838d0e409112fb98206762f4a
aomkirby123 <aomkirby123@gmail.com>
2019-03-08 23:53:52 +0700

    Resoltion 256x256 to reduce size

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ const generate = (init) => {
         ctx = canvas.getContext("2d"),
         color = document.getElementById("code").value;
 
-    canvas.width = 2048;
-    canvas.height = 2048;
+    canvas.width = 256;
+    canvas.height = 256;
 
     if(init) color = init;
     ctx.fillStyle = `#${color}`;
