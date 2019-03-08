27097c54bef57d46013f747575d59c5991fdea2a
aomkirby123 <aomkirby123@gmail.com>
2019-03-08 22:53:40 +0700

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Closer
+Generate hex code, use with [opener](https://github.com/chayapatr/opener)
---
+++ src/script.js
@@ -0,0 +1,32 @@
+const generate = (init) => {
+    let canvas = document.getElementById("canvas"),
+        ctx = canvas.getContext("2d"),
+        color = document.getElementById("code").value;
+
+    if(init) color = init;
+    ctx.fillStyle = `#${color}`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    document.getElementById("result").src = document.getElementById("canvas").toDataURL("image/png");
+}
+
+document.getElementById("code").onkeydown = event => {
+    console.log(event.keyCode);
+    if(event.target.value.length >= 6 && (event.keyCode !== 8 && event.keyCode !== 9)) event.target.blur();
+    if(event.keyCode !== 65 && event.keyCode !== 66 && event.keyCode !== 67 && event.keyCode !== 68 &&
+        event.keyCode !== 69 && event.keyCode !== 70){
+        return ( event.ctrlKey || event.altKey 
+            || (47<event.keyCode && event.keyCode<58 && event.shiftKey==false) 
+            || (95<event.keyCode && event.keyCode<106)
+            || (event.keyCode==8) || (event.keyCode==9) 
+            || (event.keyCode>34 && event.keyCode<40) 
+            || (event.keyCode==46)
+        )
+    }
+}
+
+document.getElementById("code").onchange = evt => {
+    generate();
+}
+
+generate("000000");
