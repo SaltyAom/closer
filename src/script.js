fbe852c76a76b8b4182f36ecbf8e5d46e7ea8cba
aomkirby123 <aomkirby123@gmail.com>
2019-03-08 23:49:47 +0700

    Try 2048 according to Facebook's image compression guide

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ const generate = (init) => {
         ctx = canvas.getContext("2d"),
         color = document.getElementById("code").value;
 
-    canvas.width = 720;
-    canvas.height = 720;
+    canvas.width = 2048;
+    canvas.height = 2048;
 
     if(init) color = init;
     ctx.fillStyle = `#${color}`;
